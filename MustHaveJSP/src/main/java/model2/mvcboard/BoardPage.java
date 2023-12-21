40e896c2144d55b907ab4d7f867f2b975b6030d8
pnusw14 <dustofjade@gmail.com>
2023-12-21 13:19:29 +0900

    23.12.21

---
+++ MustHaveJSP/src/main/java/model2/mvcboard/BoardPage.java
@@ -0,0 +1,13 @@
+package model2.mvcboard;
+
+public class BoardPage {
+
+	public static String pagingStr(int totalCnt, int pageSize, int blockPage, int pageNum, String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+
+}
